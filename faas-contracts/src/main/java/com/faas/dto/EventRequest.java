24195f7198f42712a5214d280aa1f091b264cf7f
mng2214 <mng2214@gmail.com>
2025-11-22 09:35:28 -0600

    clean up

--- faas-contracts/src/main/java/com/faas/dto/EventRequest.java
+++ faas-contracts/src/main/java/com/faas/dto/EventRequest.java
@@ -1,22 +1,18 @@
 package com.faas.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Map;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EventRequest {
 
     private String eventId;
     private String functionName;
     private Map<String, Object> payload;
 
-    public EventRequest() {}
-
-    public EventRequest(String eventId, String functionName, Map<String, Object> payload) {
-        this.eventId = eventId;
-        this.functionName = functionName;
-        this.payload = payload;
-    }
-
-    public String getEventId() { return eventId; }
-    public String getFunctionName() { return functionName; }
-    public Map<String, Object> getPayload() { return payload; }
 }
